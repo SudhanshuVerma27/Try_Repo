f6d2b8cee92a9d42177c549e687f11c7e1f8d90c
SudhanshuVerma27 <user14@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-06-21 09:08:17 +0000

    new

---
+++ calculator.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+int main()
+	{
+	int a,b,choice,r;
+	printf("Enter two number\n");
+	scanf("%d%d",&a,&b);
+	printf("\n Enter Choice : \n 1.Add \n 2.Subtract \n 3. multiply \n 4.Divide \n 5.Bitwise AND \n 6.Bitwise OR \n 7.Bitwise XOR\n");
+	scanf("%d" , &choice);
+	switch(choice){
+		case 1 : printf("%d + %d = %d\n" , a, b,a+b);
+			 break;
+		case 2 : printf("%d - %d = %d\n" , a, b,a-b);
+			  break;
+		case 3 : printf("%d * %d = %d\n" , a, b,a*b);
+			  break;
+		case 4 : printf("%d / %d = %d\n" , a, b,a/b);
+			  break;
+		case 5 : printf("%d & %d = %d\n" , a, b,a&b);
+                         break;
+		case 6 : printf("%d | %d = %d\n" , a, b,a|b);
+                         break;
+		case 7 : printf("%d ^ %d = %d\n" , a, b,a^b);
+                         break;
+
+	}
+	return 0;
+	}
