b14e7d6d02237710c55fe4f2df4a32a167f4b951
SudhanshuVerma27 <75315186+SudhanshuVerma27@users.noreply.github.com>
2024-07-04 16:45:34 +0530

    Update delete_adjacent_consequtive_character.c

--- test/delete_adjacent_consequtive_character.c
+++ test/delete_adjacent_consequtive_character.c
@@ -1,70 +1,45 @@
 #include<stdio.h>
 #include<string.h>
-
+void rm(char *str)
+{
+    int i;
+    int l = strlen(str);
+    int j=0;
+    for(i=0 ; i<l ; i++)
+    {
+        if(strcmp(str[i] , str[i+1]) == -1)
+        {
+            str[j++] = str[i];
+        }
+        else
+        {
+            while(str[i] == str[i+1])
+            {
+                i++;
+            }
+        }
+    }
+    str[j] = '\0';
+    
+    if(j != l)
+    {
+        rm(str);
+    }
+    // else 
+    // {
+    //     return;
+    // }
+    
+}
 int main()
 {
-	char str[10];
-	printf("Enter string :\n");
-	fgets(str , sizeof(str) , stdin);
-
-	str[strcspn(str,"\n")] = '\0';
-
-	/* for(int i=0 ; i < strlen(str)-1 ; i++)
-	{	int j;
-		char c;
-		if( str[i] - str[i+1] == -1)
-		{	c = str[i+1]; 
-			j = i;
-			while(j < strlen(str) - 2)
-			{
-				str[j] = str[j+2];
-				j++;
-			}
-			str[j] = '\0';
-		}
-		if(str[i+1] - c == -1)
-		{
-			c = str[i+1];
-                        j = i;
-                        while(j < strlen(str) - 2)
-                        {
-                                str[j] = str[j+2];
-                                j++;
-                        }
-                        str[j] = '\0';
-
-		}
-		else
-		{
-			c = ' ';
-		}
-	} */
-	
-	
-	int flag = 0;
-	flag = 0;
-		for(int i=0 ; i< strlen(str) - 1 ; i++)
-		{	
-			int j;
-			if(str[i] - str[i+1] == -1)
-			{
-				j = i;
-				flag = 1;
-				while(j < strlen(str) - 2)
-                       		 {
-                               		 str[j] = str[j+2];
-                               		 j++;
-			         }
-			
-			}
-			str[j] = '\0';
-	}
-
-
-	for(int i=0 ; i<strlen(str) ; i++)
-	{
-		printf("%c" , str[i]);
-	}
-	return 0;
+    
+    
+    char str[30];
+    printf("Enter string\n");
+    fgets(str , sizeof(str) , stdin);
+    
+    rm(str);
+    puts(str);
+    return 0;
 }
-
